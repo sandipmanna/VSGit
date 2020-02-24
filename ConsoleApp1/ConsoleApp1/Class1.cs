8dc3ca98c4405c1465383c726ee78ad914d01a31
Sandip Manna <sandip.manna@siemens-healthineers.com>
2020-02-24 16:24:36 +0530

    Add Function has been Added

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public int Add()
+        {
+            int i = 10;
+            int j = 20;
+            return i + j;
+        }
+    }
+}
